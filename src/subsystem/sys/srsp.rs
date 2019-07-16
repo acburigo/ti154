5508e0cf4fb4fdbd4ae74d50d035fe0d6adaec58
Arthur Crippa Búrigo <arthurcburigo@gmail.com>
2019-07-16 13:57:38 -0300

    Fix naming convention.

--- src/subsystem/sys/srsp.rs
+++ src/subsystem/sys/srsp.rs
@@ -6,20 +6,20 @@ use std::io::Cursor;
 use std::io::Read;
 
 #[derive(Debug)]
-pub struct PingReqSRSP {
+pub struct PingReq {
     pub capabilities: u16,
 }
 
-impl TryFrom<&mut Cursor<&[u8]>> for PingReqSRSP {
+impl TryFrom<&mut Cursor<&[u8]>> for PingReq {
     type Error = Error;
     fn try_from(cursor: &mut Cursor<&[u8]>) -> Result<Self, Error> {
         let capabilities = cursor.get_u16_le();
-        Ok(PingReqSRSP { capabilities })
+        Ok(PingReq { capabilities })
     }
 }
 
 #[derive(Debug)]
-pub struct VersionReqSRSP {
+pub struct VersionReq {
     pub transport: TransportProtocolRevision,
     pub product: ProductIdCode,
     pub major: u8,
@@ -27,7 +27,7 @@ pub struct VersionReqSRSP {
     pub maint: u8,
 }
 
-impl TryFrom<&mut Cursor<&[u8]>> for VersionReqSRSP {
+impl TryFrom<&mut Cursor<&[u8]>> for VersionReq {
     type Error = Error;
     fn try_from(cursor: &mut Cursor<&[u8]>) -> Result<Self, Error> {
         let transport = TransportProtocolRevision::try_from(Read::by_ref(cursor))?;
@@ -35,7 +35,7 @@ impl TryFrom<&mut Cursor<&[u8]>> for VersionReqSRSP {
         let major = cursor.get_u8();
         let minor = cursor.get_u8();
         let maint = cursor.get_u8();
-        Ok(VersionReqSRSP {
+        Ok(VersionReq {
             transport,
             product,
             major,
@@ -46,52 +46,52 @@ impl TryFrom<&mut Cursor<&[u8]>> for VersionReqSRSP {
 }
 
 #[derive(Debug)]
-pub struct NVCreateReqSRSP {
+pub struct NVCreateReq {
     pub status: Status,
 }
 
-impl TryFrom<&mut Cursor<&[u8]>> for NVCreateReqSRSP {
+impl TryFrom<&mut Cursor<&[u8]>> for NVCreateReq {
     type Error = Error;
     fn try_from(cursor: &mut Cursor<&[u8]>) -> Result<Self, Error> {
         let status = Status::try_from(Read::by_ref(cursor))?;
-        Ok(NVCreateReqSRSP { status })
+        Ok(NVCreateReq { status })
     }
 }
 
 #[derive(Debug)]
-pub struct NVDeleteReqSRSP {
+pub struct NVDeleteReq {
     pub status: Status,
 }
 
-impl TryFrom<&mut Cursor<&[u8]>> for NVDeleteReqSRSP {
+impl TryFrom<&mut Cursor<&[u8]>> for NVDeleteReq {
     type Error = Error;
     fn try_from(cursor: &mut Cursor<&[u8]>) -> Result<Self, Error> {
         let status = Status::try_from(Read::by_ref(cursor))?;
-        Ok(NVDeleteReqSRSP { status })
+        Ok(NVDeleteReq { status })
     }
 }
 
 #[derive(Debug)]
-pub struct NVLengthReqSRSP {
+pub struct NVLengthReq {
     pub length: u32,
 }
 
-impl TryFrom<&mut Cursor<&[u8]>> for NVLengthReqSRSP {
+impl TryFrom<&mut Cursor<&[u8]>> for NVLengthReq {
     type Error = Error;
     fn try_from(cursor: &mut Cursor<&[u8]>) -> Result<Self, Error> {
         let length = cursor.get_u32_le();
-        Ok(NVLengthReqSRSP { length })
+        Ok(NVLengthReq { length })
     }
 }
 
 #[derive(Debug)]
-pub struct NVReadReqSRSP {
+pub struct NVReadReq {
     pub status: Status,
     pub length: u8,
     pub data: Vec<u8>,
 }
 
-impl TryFrom<&mut Cursor<&[u8]>> for NVReadReqSRSP {
+impl TryFrom<&mut Cursor<&[u8]>> for NVReadReq {
     type Error = Error;
     fn try_from(cursor: &mut Cursor<&[u8]>) -> Result<Self, Error> {
         let status = Status::try_from(Read::by_ref(cursor))?;
@@ -102,7 +102,7 @@ impl TryFrom<&mut Cursor<&[u8]>> for NVReadReqSRSP {
             .read_to_end(&mut data)
             .map_err(|_| Error::NotEnoughBytes)?;
 
-        Ok(NVReadReqSRSP {
+        Ok(NVReadReq {
             status,
             length,
             data,
@@ -111,40 +111,40 @@ impl TryFrom<&mut Cursor<&[u8]>> for NVReadReqSRSP {
 }
 
 #[derive(Debug)]
-pub struct NVWriteReqSRSP {
+pub struct NVWriteReq {
     pub status: Status,
 }
 
-impl TryFrom<&mut Cursor<&[u8]>> for NVWriteReqSRSP {
+impl TryFrom<&mut Cursor<&[u8]>> for NVWriteReq {
     type Error = Error;
     fn try_from(cursor: &mut Cursor<&[u8]>) -> Result<Self, Error> {
         let status = Status::try_from(Read::by_ref(cursor))?;
-        Ok(NVWriteReqSRSP { status })
+        Ok(NVWriteReq { status })
     }
 }
 
 #[derive(Debug)]
-pub struct NVUpdateReqSRSP {
+pub struct NVUpdateReq {
     pub status: Status,
 }
 
-impl TryFrom<&mut Cursor<&[u8]>> for NVUpdateReqSRSP {
+impl TryFrom<&mut Cursor<&[u8]>> for NVUpdateReq {
     type Error = Error;
     fn try_from(cursor: &mut Cursor<&[u8]>) -> Result<Self, Error> {
         let status = Status::try_from(Read::by_ref(cursor))?;
-        Ok(NVUpdateReqSRSP { status })
+        Ok(NVUpdateReq { status })
     }
 }
 
 #[derive(Debug)]
-pub struct NVCompactReqSRSP {
+pub struct NVCompactReq {
     pub status: Status,
 }
 
-impl TryFrom<&mut Cursor<&[u8]>> for NVCompactReqSRSP {
+impl TryFrom<&mut Cursor<&[u8]>> for NVCompactReq {
     type Error = Error;
     fn try_from(cursor: &mut Cursor<&[u8]>) -> Result<Self, Error> {
         let status = Status::try_from(Read::by_ref(cursor))?;
-        Ok(NVCompactReqSRSP { status })
+        Ok(NVCompactReq { status })
     }
 }
